c75ee3829c60715830dc8e4de808a9bcefbedd0f
rachelpark <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>
2022-08-02 02:34:53 +0900

    settings : globalstyle 적용

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,17 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
-import { Router } from './Router';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
+import { Router } from "./Router";
+import reportWebVitals from "./reportWebVitals";
+import { GlobalStyle } from "./styles/Global";
 
-const container = document.getElementById('root')!;
+const container = document.getElementById("root")!;
 const root = createRoot(container);
 
 root.render(
   <Provider store={store}>
+    <GlobalStyle />
     <Router />
   </Provider>
 );
