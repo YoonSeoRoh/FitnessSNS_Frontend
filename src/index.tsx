f4ee57cf8ed10a0133b0ec4d3893c92c7d901481
rachelpark <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>
2022-08-02 00:59:03 +0900

    settings : index, store 세팅

--- src/index.tsx
+++ src/index.tsx
@@ -2,16 +2,15 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
-import App from './App';
+import { Router } from './Router';
 import reportWebVitals from './reportWebVitals';
-import './index.css';
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
 
 root.render(
   <Provider store={store}>
-    <App />
+    <Router />
   </Provider>
 );
 
