80601826ce6a76875d862fb453312b677dd9a552
rachelpark <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>
2022-08-27 21:47:40 +0900

    feat: header 영역 마크업 추가(진행중)

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,16 +1,44 @@
+/** @jsxImportSource @emotion/react */
 import React from "react";
-import { jsx, css } from "@emotion/react";
+import { css, jsx, useTheme } from "@emotion/react";
+import styled from "@emotion/styled";
 
-const divStyle = css`
-  background-color: hotpink;
-  font-size: 24px;
-  border-radius: 4px;
-  padding: 32px;
-  text-align: center;
-  &:hover {
-    color: white;
-  }
+const navStyle = css`
+  display: flex;
+  justify-content: space-between;
+  height: 58px;
 `;
+
+const Point = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  padding: 10px 14px;
+
+  width: 81px;
+  height: 35px;
+
+  border: 1px solid #dddddd;
+  border-radius: 30px;
+
+  box-sizing: border-box;
+`;
+
+const Menu = styled.div``;
+
 export const Header = () => {
-  return <nav css={divStyle}>메뉴</nav>;
+  return (
+    <nav css={navStyle}>
+      <Point>
+        <img
+          src="/assets/common/coing.png"
+          css={`
+            object-fit: ;
+          `}
+        />
+        1,200
+      </Point>
+      <Menu></Menu>
+    </nav>
+  );
 };
