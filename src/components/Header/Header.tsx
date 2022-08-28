f64c5aad2dffc361c0ebaaa1455746e2953dd361
rachelpark <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>
2022-08-28 19:57:46 +0900

    modify: header style 수정

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,18 +2,20 @@
 import React from "react";
 import { css } from "@emotion/react";
 import styled from "@emotion/styled";
+import { pointer } from "@testing-library/user-event/dist/types/pointer";
 
 const navStyle = css`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  padding: 0 20px;
+  padding: 0 21px;
   height: 58px;
 `;
 
 const Point = styled.div`
   display: flex;
   flex-direction: row;
+  justify-content: space-between;
   align-items: flex-start;
   padding: 10px 14px;
 
@@ -24,23 +26,33 @@ const Point = styled.div`
   border-radius: 30px;
 
   box-sizing: border-box;
+
+  .myPoint {
+    font-family: "Pretendard";
+    font-style: normal;
+    font-weight: 500;
+    font-size: 13px;
+    line-height: 17px;
+    text-align: right;
+  }
 `;
 
 const Menu = styled.div`
   width: 48px;
   margin-left: 17px;
   background: url("/images/menu.png") no-repeat 100%;
+  cursor: pointer;
 `;
 
 export const Header = () => {
   return (
     <nav css={navStyle}>
-      <p>로고</p>
+      <p css={{ cursor: "pointer" }}>로고</p>
       <div css={{ display: "flex" }}>
         <Point>
           <img
             src={require("../../assets/common/coin.png")}
-            css={{ width: "12px", height: "12px", objectFit: "contain" }}
+            css={{ heihgt: "15px", objectFit: "contain" }}
           />
           <p className="myPoint">1,200</p>
         </Point>
