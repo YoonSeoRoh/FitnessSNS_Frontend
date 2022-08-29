27d3ce1d9654e10ea2c144b8a228ff4701cdd7bc
rachelpark <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>
2022-08-30 07:38:46 +0900

    feat: footer 공통컴포넌트 마크업

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,50 @@
+/** @jsxImportSource @emotion/react */
 import React from "react";
+import { css } from "@emotion/react";
+
+const footStyle = css`
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+
+  div {
+    padding-top: 20px;
+    text-align: center;
+    cursor: pointer;
+
+    &:nth-child(3) {
+      padding-top: 14px;
+    }
+  }
+`;
 
 export const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div css={footStyle}>
+      <div>
+        <a>
+          <img src="/images/recom.svg" />
+        </a>
+      </div>
+      <div>
+        <a>
+          <img src="/images/store.svg" />
+        </a>
+      </div>
+      <div>
+        <a>
+          <img src="/images/runStart.svg" />
+        </a>
+      </div>
+      <div>
+        <a>
+          <img src="/images/my.svg" />
+        </a>
+      </div>
+      <div>
+        <a>
+          <img src="/images/setting.svg" />
+        </a>
+      </div>
+    </div>
+  );
 };
