0bae7c3c2ca8c1dde30c440074bf88bd097af785
rachelpark <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>
2022-08-30 00:51:53 +0900

    modify: 메인화면 마크업(진행중)

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,15 +1,15 @@
 /** @jsxImportSource @emotion/react */
 import React from "react";
 import { css } from "@emotion/react";
-import styled from "@emotion/styled";
 
-const TabMenu = styled.div`
+const tabStyle = css`
   height: 54px;
 
   ul {
     display: flex;
     justify-content: flex-start;
     padding: 21px;
+    padding-bottom: 0;
   }
 
   li {
@@ -37,27 +37,173 @@ const TabMenu = styled.div`
       height: 3px;
       background: #333333;
       align-self: stretch;
+      z-index: 2;
     }
   }
 `;
 
 const mainWrapper = css`
-  position: "absolute";
-  width: "335px";
-  height: "412px";
-  left: "20px";
-  top: "171px";
-
-  background: "#fff";
-  borderadius: "10px";
+  display: grid;
+  position: relative;
+  height: 581px;
+  background-color: #f5f5f8;
 `;
-const Graph = styled.div``;
-const List = styled.div``;
+
+const mainStyle = css`
+  position: absolute;
+  top: 14px;
+  left: 20px;
+  width: 335px;
+  height: 417px;
+  background: #ffffff;
+  box-shadow: 0px 10px 20px rgba(0, 0, 0, 0.05);
+  border-radius: 10px;
+`;
+
+const graphStyle = css`
+  position: absolute;
+  left: 50%;
+  top: 37px;
+  transform: translateX(-50%);
+  width: 222px;
+  height: 222px;
+  border-radius: 50%;
+  background-image: linear-gradient(233.11deg, #21c97c 10.65%, #87e985 88.69%),
+    linear-gradient(63.34deg, #b4d6ff -1.11%, #c275ff 48.67%, #00de79 92.59%);
+  z-index: 0;
+
+  :after {
+    content: "";
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    width: 204px;
+    height: 204px;
+    border-radius: 50%;
+    background-color: #fff;
+    z-index: 1;
+  }
+`;
+
+const innerWrapper = css`
+  position: absolute;
+  width: 190px;
+  height: 190px;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 2;
+  border-radius: 50%;
+  border: 2px dashed #dddddd;
+`;
+
+const timeImgStyle = css`
+  display: block;
+  width: 36px;
+  height: 36px;
+  margin: 23px auto 16px;
+  object-fit: fill;
+`;
+
+const textStyle = css`
+  font-family: "Pretendard";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 100%;
+
+  text-align: center;
+  letter-spacing: -0.25px;
+
+  color: #666666;
+`;
+
+const timeStyle = css`
+  margin: 8px auto 16px;
+  font-family: "Pretendard";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 28px;
+
+  text-align: center;
+
+  color: #333333;
+`;
+
+const caloryStyle = css`
+  font-family: "Pretendard";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 18px;
+
+  text-align: center;
+
+  color: #888888;
+
+  .unit {
+    padding-left: 2px;
+    font-weight: 500;
+    font-size: 14px;
+  }
+`;
+
+const btnCommon = css`
+  position: absolute;
+  left: 20px;
+  bottom: 67px;
+  width: 295px;
+  height: 58px;
+
+  font-weight: 700;
+  font-size: 16px;
+  color: #fff;
+  /* Primary */
+
+  background: #00bf68;
+  border-radius: 10px;
+  border: none;
+
+  &.btnStart {
+    background: #00bf68 url("/images/start.svg") no-repeat 91px center;
+  }
+`;
+const recordTodo = css`
+  position: absolute;
+  left: 50%;
+  transform: translateX(-50%);
+  bottom: 33px;
+  font-family: "Pretendard";
+  font-weight: 500;
+  font-size: 14px;
+
+  letter-spacing: -0.25px;
+  color: #666666;
+
+  :after {
+    content: "";
+    position: absolute;
+    left: 0;
+    bottom: -2px;
+    width: 78px;
+    height: 1px;
+    background: #999999;
+  }
+`;
+
+const listStyle = css`
+  width: 335px;
+  height: 54px;
+
+  background: #ffffff;
+  box-shadow: 0px 10px 20px rgba(0, 0, 0, 0.05);
+  border-radius: 10px;
+`;
 
 export const Main = () => {
   return (
     <div>
-      <TabMenu>
+      <div css={tabStyle}>
         <ul>
           <li className="active">
             <a>러닝기록</a>
@@ -69,29 +215,37 @@ export const Main = () => {
             <a>챌린지</a>
           </li>
         </ul>
-      </TabMenu>
-      <main css={{ backgroundColor: "#F5F5F8" }}>
-        <div css={mainWrapper}>
-          <Graph>
-            <p>시간</p>
-            <p>칼로리</p>
-          </Graph>
+      </div>
+      <main css={mainWrapper}>
+        <div css={mainStyle}>
+          <div css={graphStyle}>
+            <div css={innerWrapper}>
+              <img src="/images/time.svg" css={timeImgStyle} />
+              <p css={textStyle}>오늘의 운동 시간</p>
+              <p css={timeStyle}>00:31:34</p>
+              <p css={caloryStyle}>
+                120<span className="unit">Kcal</span>
+              </p>
+            </div>
+          </div>
+          <button css={btnCommon} className="btnStart">
+            <span css={{ paddingLeft: 28 }}>운동 시작하기</span>
+          </button>
+          <p css={recordTodo}>오늘 운동 기록</p>
         </div>
-        <button>운동 시작하기</button>
-        <button>기록보기 &gt;</button>
+        <div>
+          <ul>
+            <li css={listStyle}>
+              <img src="/images/chart.svg" />
+              <a>나의 운동기록</a>
+            </li>
+            <li css={listStyle}>
+              <a>진행 중인 챌린지</a>
+              <img />
+            </li>
+          </ul>
+        </div>
       </main>
-      <List>
-        <ul>
-          <li>
-            <a>나의 운동기록</a>
-            <img />
-          </li>
-          <li>
-            <a>진행 중인 챌린지</a>
-            <img />
-          </li>
-        </ul>
-      </List>
     </div>
   );
 };
