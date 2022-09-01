7412093718850536c78e76c2568cab0114b59984
YoonSeoRoh <96511110+YoonSeoRoh@users.noreply.github.com>
2022-09-01 10:55:27 +0900

    Revert "form 작업 & Button 컴포넌트 작업"

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,50 +1,5 @@
-/** @jsxImportSource @emotion/react */
 import React from "react";
-import { css } from "@emotion/react";
-
-const footStyle = css`
-  display: grid;
-  grid-template-columns: repeat(5, 1fr);
-
-  div {
-    padding-top: 20px;
-    text-align: center;
-    cursor: pointer;
-
-    &:nth-child(3) {
-      padding-top: 14px;
-    }
-  }
-`;
 
 export const Footer = () => {
-  return (
-    <div css={footStyle}>
-      <div>
-        <a>
-          <img src="/images/recom.svg" />
-        </a>
-      </div>
-      <div>
-        <a>
-          <img src="/images/store.svg" />
-        </a>
-      </div>
-      <div>
-        <a>
-          <img src="/images/runStart.svg" />
-        </a>
-      </div>
-      <div>
-        <a>
-          <img src="/images/my.svg" />
-        </a>
-      </div>
-      <div>
-        <a>
-          <img src="/images/setting.svg" />
-        </a>
-      </div>
-    </div>
-  );
+  return <div>Footer</div>;
 };
